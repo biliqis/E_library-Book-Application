2723185cb510a5ab59b67dfe1b4230df8fd3eb5a
biliqis <biliqistemmy@gmail.com>
2022-01-03 19:15:54 +0100

    dist

---
+++ dist/_nuxt/221ca2b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{621:function(n,t,e){"use strict";e.r(t);var l={layout:"blank",mounted:function(){this.$router.push({path:"/login"})}},o=e(63),component=Object(o.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
