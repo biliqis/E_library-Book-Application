ffabc59231a69d2368e9a254e6a29a3374934ea4
biliqis <biliqistemmy@gmail.com>
2022-01-05 19:56:20 +0100

    dist updated

---
+++ dist/_nuxt/static/1641408818/admins/register/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/admins/register", {data:[{}],fetch:{},mutations:[]});
---
+++ dist/_nuxt/static/1641408818/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fpassword\u002Freset","\u002Fpassword\u002Femail","\u002Flogin","\u002Fregister","\u002Fadmins\u002Fregister","\u002Fpassword\u002Fchange-password"]})
