309d21894998a76575abb61832ef700640cf9da2
biliqis <biliqistemmy@gmail.com>
2022-01-05 19:47:28 +0100

    changes

--- dist/_nuxt/static/1641407735/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fpassword\u002Fchange-password","\u002Flogin","\u002Fregister","\u002Fpassword\u002Femail","\u002Fadmins\u002Fregister","\u002Fpassword\u002Freset"]})
