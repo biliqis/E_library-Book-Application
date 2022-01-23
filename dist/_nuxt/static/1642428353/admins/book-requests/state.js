1299ca9f94469ac43ff16ac3439be8861e9ae7b3
biliqis <biliqistemmy@gmail.com>
2022-01-23 12:13:50 +0100

    final-commit

--- dist/_nuxt/static/1642428353/admins/book-requests/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642428353",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fadmins\u002Fbook-requests",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
